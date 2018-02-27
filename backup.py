1324cf76742286e5f03801693cd90becb5ad898d
Aldarien <aldarien85@gmail.com>
2018-02-27 11:56:46 -0300

    Cleanup

--- backup.py
+++
@@ -1,4 +0,0 @@
-command = 'mysqldump -uroot -paryafoole --no-create-info --skip-triggers --result-file="%s" %s'.format(
-    config('app.directories.temp'),
-    config('app.databases.source.name')
-    )
